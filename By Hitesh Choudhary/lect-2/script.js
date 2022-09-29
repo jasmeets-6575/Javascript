8f9416dcec80b02e0a1a7c12ff59c012e0277402
Jasmeet Singh <jasmeetsonu990@gmail.com>
2022-09-30 00:35:18 +0530

    lecture 2

---
+++ By Hitesh Choudhary/lect-2/script.js
@@ -0,0 +1,17 @@
+const uid = "abc123"
+
+var fullName = "Hello world"
+var email = "xyzabc@gh.com"
+var password = "12345"
+
+// fullName = prompt("enter your name"); 
+
+console.log(fullName);
+console.log("Email of the user is " + email);
+console.log(uid);
+
+console.log(`
+    with Unique Id : ${uid}
+    Email of the user is : ${email}
+    Name of the user is : ${fullName}
+`)
