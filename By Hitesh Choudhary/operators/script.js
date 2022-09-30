1be21bb577fde03cafd29e6728e143af72eb0783
Jasmeet Singh <jasmeetsonu990@gmail.com>
2022-10-01 00:49:05 +0530

    Operators in JS

---
+++ By Hitesh Choudhary/operators/script.js
@@ -0,0 +1,19 @@
+ var num1 = 7;
+ var num2 = 6;
+
+ console.log(num1+num2);
+ 
+ var answer = num1>num2;
+ console.log(answer);
+
+//  And also we can find the discount percentage 
+
+var sp=199;
+var lp=799;
+var d=((lp-sp)/lp)*100; 
+
+console.log( "Discount Percentage is :" + d);
+displayD=Math.round(d);
+console.log(displayD + "% off")
+
+console.log("this is  " + sp);
