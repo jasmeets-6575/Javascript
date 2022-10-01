f0aa6136c44286242da61940cee3c40ce2c7c849
Jasmeet Singh <jasmeetsonu990@gmail.com>
2022-10-01 23:09:59 +0530

    Context in JS

---
+++ By Hitesh Choudhary/context/script.js
@@ -0,0 +1,14 @@
+// sayHello();
+
+// function sayHello() {
+//     console.log("Hello world");
+// }
+
+if(2===2){
+    console.log("this is true");
+}
+
+var myName="JS"
+if(myName=== window.myName){
+    console.log("This is also a true statement");
+}
