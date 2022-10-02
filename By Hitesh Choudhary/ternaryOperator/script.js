df2fcc06f5853725a90132a6245cdb2800eb2f76
Jasmeet Singh <jasmeetsonu990@gmail.com>
2022-10-02 16:11:30 +0530

    ternary Operator

---
+++ By Hitesh Choudhary/ternaryOperator/script.js
@@ -0,0 +1,6 @@
+// Show User a button to Sign out if he or she is authenticated 
+// Otherwise Show them a Option to login or sign up
+
+var authenticated =false;
+
+authenticated ? console.log("Sign Out button"):console.log("Login");
