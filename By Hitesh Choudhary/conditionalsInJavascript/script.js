2ff59e23394b9689980fffaa9c88c1706d39d1a3
Jasmeet Singh <jasmeetsonu990@gmail.com>
2022-10-05 16:37:28 +0530

    Conditionals

---
+++ By Hitesh Choudhary/conditionalsInJavascript/script.js
@@ -0,0 +1,13 @@
+var temperature;
+
+temperatur=35;
+
+if (temperature<20) {
+    console.log("It's cold outside .");
+}
+
+if (temperature<30){
+    console.log("It's moderate outside .")
+} else{
+    console.log("It's Hot Outside .")
+}
