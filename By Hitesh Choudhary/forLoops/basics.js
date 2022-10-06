afc2c3fc3fe2b318d75bc50be43005d267485dca
Jasmeet Singh <jasmeetsonu990@gmail.com>
2022-10-07 04:01:32 +0530

    For loops

---
+++ By Hitesh Choudhary/forLoops/basics.js
@@ -0,0 +1,20 @@
+// for (let i=0; i<=10; i++) {
+//     console.log(i);
+// }
+
+
+const states = [
+    "jk",
+    "pb",
+    "hp",
+    1947,
+    "uk",
+    "dl",
+    1960,
+    "mh",
+];
+for (i=0;i<states.length;i++) {
+    if (typeof states[i] !== "string") 
+        continue;
+        console.log(states[i]);
+};
