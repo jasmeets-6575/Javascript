b19781089aad81d569abed130cc846c944428b35
Jasmeet Singh <jasmeetsonu990@gmail.com>
2022-10-14 00:09:20 +0530

    newKeyword

---
+++ By Hitesh Choudhary/newKeyword/script.js
@@ -0,0 +1,22 @@
+var user = function (firstName, courseCount) {
+    this.firstName=firstName;
+    this.courseCount=courseCount;
+    this.getcourseCount= function() {
+        console.log(`Course Count is : ${this.courseCount}`);
+    };
+};
+
+user.prototype.getFirstName = function () {
+    console.log(`Yoou first name is : ${this.firstName}`);
+};
+
+
+var john = new user ("john", 2);
+john.getcourseCount();
+john.getFirstName();
+// console.log(john); 
+
+var sham = new user ("sham",4);
+sham.getcourseCount();
+sham.getFirstName();
+// console.log(sham);
