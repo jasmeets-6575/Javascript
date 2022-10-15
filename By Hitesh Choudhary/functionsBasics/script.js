e182f04258c3d9302dfd4c65d5e840534b7947fc
Jasmeet Singh <jasmeetsonu990@gmail.com>
2022-10-15 20:11:39 +0530

    functionsBasics/

---
+++ By Hitesh Choudhary/functionsBasics/script.js
@@ -0,0 +1,17 @@
+function sayHello (name){
+    console.log("My name is Mr ");
+    console.log(`My name is ${name} How are you ??`);
+}
+
+// sayHello("RAM");
+// sayHello("SHAM");
+
+
+function namastey() {
+    return "Hello in India";
+}
+
+var greetings = namastey();
+
+console.log(greetings);
+console.log(namastey());
