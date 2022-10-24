99dee96492bd34b768602857cbf4060b7df473c1
Jasmeet Singh <jasmeetsonu990@gmail.com>
2022-10-24 12:24:44 +0530

    number Additionals

---
+++ By John Smilga/datatypes/app.js
@@ -0,0 +1,5 @@
+// Data Types - 7 Total
+// Primitive - String , Number , Boolean , Null , undefined , Symbol(ES6)
+// Object - Arrays , Functions , Objects
+
+// typeof - operator (typeof variable) (typeof value)
