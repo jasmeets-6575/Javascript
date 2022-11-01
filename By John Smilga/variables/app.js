c24d861d839b726a5b112cdf8d8aab738deb3236
Jasmeet Singh <jasmeetsonu990@gmail.com>
2022-11-02 01:56:22 +0530

    Variables

---
+++ By John Smilga/variables/app.js
@@ -0,0 +1,12 @@
+// Variables - Store , Access , Modify=== Value 
+// Declare , Assignment Operator ,Assign Value
+
+
+let name = "Jass" ;
+// console.log(name);
+
+let address, zip, state;
+address = "1010 wall street";
+zip = "2323";
+state= "YUPSS";
+console.log(address, zip,state);
