1fe82b72172b2e7cb697033fd468b8f5bb0cc4f7
Jasmeet Singh <jasmeetsonu990@gmail.com>
2022-11-03 22:11:21 +0530

    for loop

---
+++ By John Smilga/loops/for.js
@@ -0,0 +1,10 @@
+// For loop
+
+// let i;
+// for (i=0 ; i < 10 ; i++){
+//     console.log("and the number is " + i); 
+// }
+
+for (let num=10 ; num>=0 ; num--) {
+    console.log("the number is " + num);
+}
