ae8cb8492f2ad10f1f3244d68fd03e20a2882212
Jasmeet Singh <jasmeetsonu990@gmail.com>
2022-11-03 18:32:51 +0530

    conditional

---
+++ By John Smilga/conditionalStatements/basics.js
@@ -0,0 +1,24 @@
+// const value = 2 > 1 ;
+// const value2 = 1 > 2 ;
+
+// if (value2) {
+//     console.log("HELLO WORLD");
+// }
+// else{
+//     console.log("Hello people");
+// }
+
+
+const num1 = 6;
+const num2 = 6;
+const result = num1 >= num2;
+
+if (num1 > num2) {
+    console.log("First number is bigger");
+}
+else if(result){
+    console.log("both are equal");
+}
+else{
+    console.log("second  number is bigger");
+}
---
+++ By John Smilga/conditionalStatements/equality.js
@@ -0,0 +1,10 @@
+//  === , == , !== ,!==
+
+const num1 = 6 ;
+const num2 = "6" ;
+
+const value1 = num1 == num2 ;
+const value2 = num1 === num2 ;
+
+console.log(value1);
+console.log(value2);
