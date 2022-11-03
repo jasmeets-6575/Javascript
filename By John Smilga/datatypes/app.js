45aac0178357e93d082d0d22e96e36c40cbd9c3d
Jasmeet Singh <jasmeetsonu990@gmail.com>
2022-11-03 12:51:19 +0530

    Datatypes

--- By John Smilga/datatypes/app.js
+++ By John Smilga/datatypes/app.js
@@ -3,3 +3,22 @@
 // Object - Arrays , Functions , Objects
 
 // typeof - operator (typeof variable) (typeof value)
+
+const text = "some text";
+
+const num = 45;
+
+let value1= true;
+let value2= false;
+
+const result = null ;
+
+let name ;
+
+console.log(typeof text);
+console.log(typeof num);
+console.log(typeof value1);
+console.log(typeof value2);
+console.log(typeof result);
+console.log(typeof name);
+console.log(typeof "HEllo World");
