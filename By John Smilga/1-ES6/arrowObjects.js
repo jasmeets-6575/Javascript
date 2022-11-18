2878fa420ba4ead23cff4a67d827181b14cf871d
Jasmeet Singh <jasmeetsonu990@gmail.com>
2022-11-18 15:38:12 +0530

    ES6

---
+++ By John Smilga/1-ES6/arrowObjects.js
@@ -0,0 +1,27 @@
+// Arrow Functions or Fat Arrow Functions
+// 'this' keyword
+
+const bob = {
+    firstName: "bob",
+    lastName: "smith",
+    sayName: function() {
+      console.log(this);
+      setTimeout(() => {
+        console.log(this);
+        console.log(`Hello, my name is ${this.firstName} ${this.lastName}`);
+      }, 1000);
+    },
+  };
+  
+  const anna = {
+    firstName: "anna",
+    lastName: "sanders",
+    sayName: () => {
+      console.log(this);
+      console.log(`Hello, my name is ${this.firstName} ${this.lastName}`);
+    },
+  };
+  
+  bob.sayName();
+  anna.sayName();
+  
