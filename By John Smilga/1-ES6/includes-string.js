6c82b43c361862f6b874016f4459276f9b9261c7
Jasmeet Singh <jasmeetsonu990@gmail.com>
2022-11-26 19:35:11 +0530

    strings include

---
+++ By John Smilga/1-ES6/includes-string.js
@@ -0,0 +1,29 @@
+// String includes() - checks if a string contains another string.
+
+const products = [
+    { title: 'Modern Poster' },
+    { title: 'Bar Stool' },
+    { title: 'Armchair' },
+    { title: 'Leather Chair' },
+  ];
+  
+  const text = 'a';
+  
+  const filteredProducts = products.filter((product) =>
+    product.title.toLowerCase().includes(text)
+  );
+  
+  console.log(filteredProducts);
+  
+  // const firstName = 'john';
+  
+  // const result = firstName.includes('j', 1);
+  
+  // const product = {
+  //   title: 'Leather Chair',
+  // };
+  
+  // const result = product.title.includes('le');
+  
+  // console.log(result);
+  
