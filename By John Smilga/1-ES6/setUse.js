39017f5addbe9fa2ef92f311bef796a15f7dbb53
Jasmeet Singh <jasmeetsonu990@gmail.com>
2022-12-02 20:43:45 +0530

    set use

---
+++ By John Smilga/1-ES6/setUse.js
@@ -0,0 +1,33 @@
+// new Set() - accepts iterable objects
+
+const products = [
+    {
+      title: 'high-back bench',
+      company: 'ikea',
+    },
+    {
+      title: 'albany table',
+      company: 'marcos',
+    },
+    {
+      title: 'accent chair',
+      company: 'caressa',
+    },
+    {
+      title: 'wooden table',
+      company: 'ikea',
+    },
+  ];
+  
+  // const companies = products.map((item) => item.company);
+  // console.log(companies);
+  
+  // const uniqueCompanies = new Set(companies);
+  // console.log(uniqueCompanies);
+  
+  // const finalCompanies = ['all', ...uniqueCompanies];
+  // console.log(finalCompanies);
+  
+  const result = ['all', ...new Set(products.map((item) => item.company))];
+  console.log(result);
+  
