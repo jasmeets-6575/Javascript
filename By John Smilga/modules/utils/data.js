f32d03f6e0ac4835f3544710137c132f236bb3fe
Jasmeet Singh <jasmeetsonu990@gmail.com>
2022-12-02 22:43:45 +0530

    default export

---
+++ By John Smilga/modules/utils/data.js
@@ -0,0 +1,7 @@
+export const random = 'random value';
+
+export const people = [
+  { name: 'john', job: 'developer' },
+  { name: 'susan', job: 'designer' },
+  { name: 'anna', job: 'the boss' },
+];
