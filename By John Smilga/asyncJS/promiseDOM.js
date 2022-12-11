85333c9ba509c2e42b82dbe8c796d438a0736144
Jasmeet Singh <jasmeetsonu990@gmail.com>
2022-12-11 23:10:25 +0530

    Dom example

---
+++ By John Smilga/asyncJS/promiseDOM.js
@@ -0,0 +1,27 @@
+// callbacks, promises, async/await
+// must have async
+// await waits till promise is settled
+// error handling - try/catch
+const heading1 = document.querySelector('.one');
+const heading2 = document.querySelector('.four');
+const heading3 = document.querySelector('.three');
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', () => {
+  addColor(1000, heading1, 'red')
+    .then(() => addColor(2000, heading2, 'green'))
+    .then(() => addColor(1000, heading3, 'blue'))
+    .catch((err) => console.log(err));
+});
+
+function addColor(time, element, color) {
+  return new Promise((resolve, reject) => {
+    if (element) {
+      setTimeout(() => {
+        element.style.color = color;
+        resolve();
+      }, time);
+    } else {
+      reject(new Error(`There is no such element ${element}`));
+    }
+  });
+}
