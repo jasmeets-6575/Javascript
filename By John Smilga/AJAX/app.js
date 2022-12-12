4251966e482d2268b06a3ecfa63ce05f1b0aec98
Jasmeet Singh <jasmeetsonu990@gmail.com>
2022-12-12 17:56:34 +0530

    ajax

---
+++ By John Smilga/AJAX/app.js
@@ -0,0 +1,20 @@
+const xhr = new XMLHttpRequest();
+
+xhr.open('GET', './api/sample.txt');
+xhr.onreadystatechange = function () {
+  // console.log(xhr);
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    const text = document.createElement('p');
+    text.textContent = xhr.responseText;
+    document.body.appendChild(text);
+  } else {
+    console.log({
+      status: xhr.status,
+      text: xhr.statusText,
+      state: xhr.readyState,
+    });
+  }
+};
+xhr.send();
+
+console.log('hello world');
