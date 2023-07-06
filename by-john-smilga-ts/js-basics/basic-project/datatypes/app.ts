19909089d837ec8d0780c97049cf8ba45ec3e383
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-06 14:05:54 +0530

    datatypes

---
+++ by-john-smilga-ts/js-basics/basic-project/datatypes/app.ts
@@ -0,0 +1,24 @@
+// Data Types - 7 Total
+// Primitive - String , Number , Boolean , Null , undefined , Symbol(ES6)
+// Object - Arrays , Functions , Objects
+
+// typeof - operator (typeof variable) (typeof value)
+
+const text = "some text";
+
+const num = 45;
+
+let value1 = true;
+let value2 = false;
+
+const result = null;
+
+let names;
+
+console.log(typeof text);
+console.log(typeof num);
+console.log(typeof value1);
+console.log(typeof value2);
+console.log(typeof result);
+console.log(typeof names);
+console.log(typeof "HEllo World");
