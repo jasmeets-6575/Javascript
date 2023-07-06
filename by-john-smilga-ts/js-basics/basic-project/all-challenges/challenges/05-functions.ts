09c49e83e848b5f2f1516c9dbc03e28ca727bc21
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-06 16:06:05 +0530

    functions challenge

---
+++ by-john-smilga-ts/js-basics/basic-project/all-challenges/challenges/05-functions.ts
@@ -0,0 +1,9 @@
+const calculateTotal = function random(subTotal, tax) {
+  return subTotal + tax;
+};
+
+const order1 = calculateTotal(100, 10);
+const order2 = calculateTotal(50, 5);
+const order3 = calculateTotal(25, 5);
+
+console.log(order1, order2, order3);
