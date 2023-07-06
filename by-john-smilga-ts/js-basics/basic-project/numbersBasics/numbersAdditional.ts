b18d52ff68493a39ef7f57b73803727ecff3b44b
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-06 13:28:27 +0530

    Numbeers

---
+++ by-john-smilga-ts/js-basics/basic-project/numbersBasics/numbersAdditional.ts
@@ -0,0 +1,12 @@
+// Numbers
+// +=,-=,/=,*=,++,--,%
+// Modules (%) Operator returns the remainder after integration division
+
+let number = 40;
+number += 5;
+number += 5;
+number += 5;
+number += 5;
+number /= 5;
+
+console.log(number);
---
+++ by-john-smilga-ts/js-basics/basic-project/numbersBasics/numbersBasic.ts
@@ -0,0 +1,10 @@
+// Numbers
+// Loosely Typed = don't declare type
+
+const number1 = 34;
+const number2 = 2.345;
+const number3 = "2.345";
+
+console.log(number);
+console.log(number2);
+console.log(number3);
