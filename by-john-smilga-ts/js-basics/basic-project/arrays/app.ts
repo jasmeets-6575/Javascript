d66bec7fc9b59b44e23f35ae88a4f75833529e82
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-06 14:51:28 +0530

    arrays

---
+++ by-john-smilga-ts/js-basics/basic-project/arrays/app.ts
@@ -0,0 +1,11 @@
+// Arrays - [] ,0 index based
+
+const friends =["john", "peter", "bob","susy", 45 ,undefined ,null ];
+
+let bestFriend = friends[2];
+friends[4] = "anna";
+
+console.log(friends);
+console.log(friends[4]);
+console.log(bestFriend);
+console.log(friends[3]);
