03ab10fcbea342e600acfacebc9b1185df6f1329
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-07 23:32:02 +0530

    map

--- by-john-smilga-ts/js-basics/basic/array-iterators/map.ts
+++ by-john-smilga-ts/js-basics/basic/array-iterators/map.ts
@@ -3,24 +3,24 @@
 // does not change the size
 // uses values from original array whne making new one
 
-const people = [
+const peep = [
   { name: "bob", age: 20, position: "developer" },
   { name: "Peter", age: 22, position: "designer" },
   { name: "Susy", age: 25, position: "Boss" },
   { name: "Anna", age: 27, position: "manager" },
 ];
 
-const ages = people.map(function (person) {
+const ages = peep.map(function (person) {
   return person.age + 20;
 });
-const newPeople = people.map(function (person) {
+const newPeople = peep.map(function (person) {
   return {
     firstName: person.name.toUpperCase(),
     oldAge: person.age - 5,
   };
 });
 
-const fnames = people.map(function (person) {
+const fnames = peep.map(function (person) {
   return `<h1>${person.name}</h1>`;
 });
 
