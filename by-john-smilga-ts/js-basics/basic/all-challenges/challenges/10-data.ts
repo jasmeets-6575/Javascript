2d09a29cb2e36e79fae95e4db74a19d9e4b182c2
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-07 20:30:11 +0530

    arrays methods challenge

---
+++ by-john-smilga-ts/js-basics/basic/all-challenges/challenges/10-data.ts
@@ -0,0 +1,36 @@
+const students = [
+  {
+    id: 1,
+    name: 'peter',
+    score: 80,
+    favoriteSubject: 'math',
+  },
+  {
+    id: 2,
+    name: 'susan',
+    score: 99,
+    favoriteSubject: 'history',
+  },
+  {
+    id: 3,
+    name: 'anna',
+    score: 85,
+    favoriteSubject: 'math',
+  },
+  {
+    id: 4,
+    name: 'john',
+    score: 34,
+    favoriteSubject: 'art',
+  },
+  {
+    id: 5,
+    name: 'bobo',
+    score: 74,
+    favoriteSubject: 'math',
+  },
+];
+
+function sayName(name) {
+  console.log(name);
+}
