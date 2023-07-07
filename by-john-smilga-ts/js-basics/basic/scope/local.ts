0089ee93400f735e29e72355f37536618fe40a38
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-07 23:41:26 +0530

    local scope

---
+++ by-john-smilga-ts/js-basics/basic/scope/local.ts
@@ -0,0 +1,10 @@
+// local Scope
+
+function calculte() {
+  const name = "john";
+  const age = 25;
+  // codes goes here
+  const becomesGlobal = "global variable";
+}
+calculte();
+// console.log(becomesGlobal);
