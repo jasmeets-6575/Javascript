7ad62400d6e7c672d85b2a269641b651a42ce60a
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-08 15:28:44 +0530

    map challenge

---
+++ by-john-smilga-ts/js-basics/basic/array-iterators-challenge/maps.ts
@@ -0,0 +1,14 @@
+const students = [
+  { id: 1, name: "Peter", score: 80, favSubject: "Maths" },
+  { id: 2, name: "Jordan", score: 75, favSubject: "Chemistry" },
+  { id: 3, name: "Bobo", score: 90, favSubject: "English" },
+  { id: 4, name: "Anna", score: 85, favSubject: "Physics" },
+  { id: 5, name: "Susy", score: 70, favSubject: "Biology" },
+];
+
+const updatedStudents = students.map(function (student) {
+  // console.log(student);
+  student.favSubject = "Chemistry";
+  return student;
+});
+console.log(updatedStudents);
