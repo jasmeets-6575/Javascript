2b2b635c1496b74f13a7f33cb9488cf725bd27b9
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-10 20:26:19 +0530

    replace child

--- by-john-smilga-ts/dom/replaceChild/app.ts
+++ by-john-smilga-ts/dom/replaceChild/app.ts
@@ -1,19 +1,12 @@
-// createElement('element')
-// createTextNode('text content')
-// element.appendChild(childElement)
-
-// replaceChild('new','old');
-
 const idResult = document.querySelector("#result") as HTMLElement;
 const redfirst = document.querySelector(".red") as HTMLElement;
-// create empty element
 
 const bodyDivs = document.createElement("div");
-// create text node
 const texts = document.createTextNode("a simple body div");
+
 bodyDiv.appendChild(texts);
 document.body.insertBefore(bodyDivs, idResult);
-// result element
+
 const headings = document.createElement("h2");
 const headingTexts = document.createTextNode("dynamic heading");
 heading.appendChild(headingTexts);
@@ -25,5 +18,5 @@ const smallHeading = document.createElement("h6");
 const smallText = document.createTextNode(`i'm small heading text`);
 smallHeading.classList.add("red");
 smallHeading.appendChild(smallText);
-document.body.replaceChild(smallHeading, bodyDiv);
+document.body.replaceChild(smallHeading, bodyDivs);
 console.log(idResult.children);
