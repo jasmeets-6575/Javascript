4c67ff16d6f299aad6775bac638760cacdffa6bc
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-10 20:26:19 +0530

    remove child

---
+++ by-john-smilga-ts/dom/removeChild/app.ts
@@ -0,0 +1,4 @@
+const rcResult = document.querySelector("result") as HTMLElement;
+const rcheading = result.querySelector("h1") as HTMLElement;
+result.removeChild(rcheading);
+console.log(rcheading);
