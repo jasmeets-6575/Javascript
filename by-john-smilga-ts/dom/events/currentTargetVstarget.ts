52becdece40c24138a78600049b7684f62dc095a
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-11 21:43:23 +0530

    changed ev.target vs ev. targetvalue

--- by-john-smilga-ts/dom/events/currentTargetVstarget.ts
+++ by-john-smilga-ts/dom/events/currentTargetVstarget.ts
@@ -1,15 +1,11 @@
-const btns = document.querySelectorAll(".btn");
+const btns = document.querySelectorAll<HTMLElement>(".btn");
 
-btns.forEach((btn) => {
-  btn.addEventListener("click", function (e) {
-    const { target } = e;
+btns.forEach((btn: HTMLElement) => {
+  btn.addEventListener("click", function (e: MouseEvent) {
     // console.log(e.currentTarget);
     // e.currentTarget.style.color = 'green';
     console.log("current", e.currentTarget);
     console.log("target", e.target);
-    if (target instanceof HTMLElement) {
-      target.style.color = "green";
-    }
-    e.target;
+    (e.target as HTMLElement).style.color = "green";
   });
 });
