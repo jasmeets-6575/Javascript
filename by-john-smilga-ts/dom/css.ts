2bfb3415cfff7c29f822c96793c85702bcd7c1fb
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-11 17:37:50 +0530

    css

--- by-john-smilga-ts/dom/css.ts
+++ by-john-smilga-ts/dom/css.ts
@@ -1,4 +1,10 @@
-// Css
+// CSS
 
-const random = document.querySelector("random")! as HTMLElement;
-console.log(random.style);
+const random = document.querySelector(".random") as HTMLElement;
+
+// random.style.backgroundColor = 'blue';
+// random.style.color = 'white';
+// random.style.fontSize = '3rem';
+// random.style.textTransform = 'capitalize';
+
+random.classList.add("title");
