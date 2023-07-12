57c1660cd45c9e2c0f5e42d124517efc9d7a5cfd
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-12 22:44:23 +0530

    modal js

---
+++ by-john-smilga-ts/projects/06-modal/app.ts
@@ -0,0 +1,15 @@
+// select modal-btn,modal-overlay,close-btn
+// listen for click events on modal-btn and close-btn
+// when user clicks modal-btn add .open-modal to modal-overlay
+// when user clicks close-btn remove .open-modal from modal-overlay
+
+const modalBtn = document.querySelector(".modal-btn") as HTMLElement;
+const modal = document.querySelector(".modal-overlay") as HTMLElement;
+const closeBtn = document.querySelector(".close-btn") as HTMLElement;
+
+modalBtn.addEventListener("click", function () {
+  modal.classList.add("open-modal");
+});
+closeBtn.addEventListener("click", function () {
+  modal.classList.remove("open-modal");
+});
