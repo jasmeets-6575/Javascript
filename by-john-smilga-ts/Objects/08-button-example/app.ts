a3f217e5c9d58883b7f504ddccc31a908e4ee11c
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-14 03:01:11 +0530

    button example

---
+++ by-john-smilga-ts/Objects/08-button-example/app.ts
@@ -0,0 +1,19 @@
+const counter = {
+  count: 0,
+  increment() {
+    console.log(this);
+    this.count++;
+    console.log(this.count);
+  },
+};
+
+const btn = document.querySelector(".increment") as HTMLButtonElement;
+
+// fail
+// btn.addEventListener('click', counter.increment);
+
+// some edge cases
+// btn.addEventListener('click', counter.increment.bind(counter));
+
+const increment = counter.increment.bind(counter);
+btn.addEventListener("click", increment);
