a6d51d0debe2c33048939f015d46c4f4e7a53860
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-14 01:06:52 +0530

    this keyword

---
+++ by-john-smilga-ts/Objects/03-this-keyword/basics.ts
@@ -0,0 +1,22 @@
+// this
+// points to the left of the dot
+
+const john = {
+  firstName: 'john',
+  lastName: 'anderson',
+  fullName: function () {
+    console.log(this);
+    console.log(`My full name is ${this.firstName} ${this.lastName}`);
+  },
+};
+const bob = {
+  firstName: 'peter',
+  lastName: 'sanders',
+  fullName: function () {
+    console.log(this);
+    console.log(`My full name is ${this.firstName} ${this.lastName}`);
+  },
+};
+
+john.fullName();
+bob.fullName();
