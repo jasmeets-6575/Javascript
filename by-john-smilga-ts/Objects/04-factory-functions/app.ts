c801a088a827e7466054677b8d26fb05de6210c5
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-14 01:22:04 +0530

    functions

---
+++ by-john-smilga-ts/Objects/04-factory-functions/app.ts
@@ -0,0 +1,39 @@
+// Blue Print
+// Factory Functions and Constructor Functions
+// Factory Functions
+
+// const john = {
+//   firstName: 'john',
+//   lastName: 'anderson',
+//   fullName: function () {
+//     console.log(
+//       `My full name is ${this.firstName} ${this.lastName} and I love JS`
+//     );
+//   },
+// };
+// const bob = {
+//   firstName: 'peter',
+//   lastName: 'sanders',
+//   fullName: function () {
+//     console.log(`My full name is ${this.firstName} ${this.lastName}`);
+//   },
+// };
+
+function createPerson(firstName, lastName) {
+  return {
+    firstName: firstName,
+    lastName: lastName,
+    fullName: function () {
+      console.log(
+        `My full name is ${this.firstName} ${this.lastName} and I love JS`
+      );
+    },
+  };
+}
+
+const john = createPerson("john", "anderson");
+john.fullName();
+const bob = createPerson("susy", "apple");
+bob.fullName();
+const susy = createPerson("bob", "jordan");
+susy.fullName();
