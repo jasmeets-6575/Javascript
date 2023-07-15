fb49116a330972140b5c197effb04f456efb6748
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-16 04:19:21 +0530

    adding data

---
+++ by-john-smilga-ts/modules/utils/data.ts
@@ -0,0 +1,12 @@
+export const random: string = "random value";
+
+export interface Person {
+  name: string;
+  job: string;
+}
+
+export const people: Person[] = [
+  { name: "john", job: "developer" },
+  { name: "susan", job: "designer" },
+  { name: "anna", job: "the boss" },
+];
