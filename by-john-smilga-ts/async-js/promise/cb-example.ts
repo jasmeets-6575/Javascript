330811cc1f05980180200c9d32d6f833cbd3edec
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-16 22:55:07 +0530

    promise

---
+++ by-john-smilga-ts/async-js/promise/cb-example.ts
@@ -0,0 +1,22 @@
+// callbacks, promises, async/await
+// PROMISES - Pending, Resolved, Rejected
+// then catch - pass another callback
+const heading1 = document.querySelector('.one');
+const heading2 = document.querySelector('.two');
+const heading3 = document.querySelector('.three');
+
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+  setTimeout(() => {
+    heading1.style.color = 'red';
+    setTimeout(() => {
+      heading2.style.color = 'green';
+      setTimeout(() => {
+        heading3.style.color = 'blue';
+      }, 1000);
+    }, 2000);
+  }, 1000);
+
+  //
+});
