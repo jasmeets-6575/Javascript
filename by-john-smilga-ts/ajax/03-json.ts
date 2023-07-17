0916143581a1518790509746360f889ecb1a11d2
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-17 22:25:30 +0530

    json data

---
+++ by-john-smilga-ts/ajax/03-json.ts
@@ -0,0 +1,19 @@
+// JSON Javascript Object Notation
+const btn = document.querySelector(".btn") as HTMLButtonElement;
+const url = "./api/people.json";
+btn.addEventListener("click", () => {
+  getData(url);
+});
+
+function getData(url) {
+  const xhr = new XMLHttpRequest();
+  console.log(xhr);
+
+  xhr.open("GET", url);
+  xhr.onreadystatechange = function () {
+    // console.log(xhr);
+    if (xhr.readyState === 4 && xhr.status === 200) {
+    }
+  };
+  xhr.send();
+}
