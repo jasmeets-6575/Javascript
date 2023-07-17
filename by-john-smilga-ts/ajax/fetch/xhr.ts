c82cd3e4d83b6f67dae0a638c9aefdf211073860
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-17 22:37:03 +0530

    xhr complete

--- by-john-smilga-ts/ajax/fetch/xhr.ts
+++ by-john-smilga-ts/ajax/fetch/xhr.ts
@@ -1,28 +1,26 @@
-// JSON Javascript Object Notation
-const btn = document.querySelector(".btn") as HTMLButtonElement;
-const url = "./api/people.json";
+const btn: HTMLButtonElement = document.querySelector(
+  ".btn"
+) as HTMLButtonElement;
+const url: string = "./api/people.json";
+
 btn.addEventListener("click", () => {
   getData(url);
 });
 
-function getData(url) {
-  const xhr = new XMLHttpRequest();
+function getData(url: string) {
+  const xhr: XMLHttpRequest = new XMLHttpRequest();
   console.log(xhr);
 
   xhr.open("GET", url);
   xhr.onreadystatechange = function () {
-    // console.log(xhr);
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      // parse
-      // stringify
-
-      const data = JSON.parse(xhr.responseText);
-      const displayData = data
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+      const data: any[] = JSON.parse(xhr.responseText);
+      const displayData: string = data
         .map((item) => {
           return `<p>${item.name}</p>`;
         })
         .join("");
-      const element = document.createElement("div");
+      const element: HTMLDivElement = document.createElement("div");
       element.innerHTML = displayData;
       document.body.appendChild(element);
     } else {
