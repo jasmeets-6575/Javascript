c037b36b7212798024ca034eab0566bb57643410
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-17 22:23:34 +0530

    getting data

---
+++ by-john-smilga-ts/ajax/02-get-data.ts
@@ -0,0 +1,26 @@
+const btn = document.querySelector(".btn") as HTMLButtonElement;
+btn.addEventListener("click", () => {
+  getData();
+});
+
+function getData() {
+  const xhr = new XMLHttpRequest();
+  console.log(xhr);
+
+  xhr.open("GET", "./api/sample.txt");
+  xhr.onreadystatechange = function () {
+    // console.log(xhr);
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const text = document.createElement("p");
+      text.textContent = xhr.responseText;
+      document.body.appendChild(text);
+    } else {
+      console.log({
+        status: xhr.status,
+        text: xhr.statusText,
+        state: xhr.readyState,
+      });
+    }
+  };
+  xhr.send();
+}
