afa0adc9d09181dbb2e524c0dd5a100f35d35a70
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-17 22:35:00 +0530

    try catch data

--- by-john-smilga-ts/ajax/fetch/fetch-async-try-catch.ts
+++ by-john-smilga-ts/ajax/fetch/fetch-async-try-catch.ts
@@ -2,10 +2,32 @@ const url = "./api/people.json";
 
 const btn = document.querySelector(".btn") as HTMLButtonElement;
 
-try {
-  console.log("hello");
-} catch (error) {
-  console.log(error);
-}
+btn.addEventListener("click", async () => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayItems(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-console.log("hey");
+const displayItems = (items) => {
+  const displayData = items
+    .map((item) => {
+      return `<p>${item.name}</p>`;
+    })
+    .join("");
+  const element = document.createElement("div");
+  element.innerHTML = displayData;
+  document.body.appendChild(element);
+};
+
+// try {
+//   console.log('hello');
+//   console.log(random);
+// } catch (error) {
+//   console.log(error);
+// }
+
+// console.log('hey');
