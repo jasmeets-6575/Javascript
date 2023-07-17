bc8cba0e484376ca9a82dc467362b8968c6c97f0
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-17 22:34:38 +0530

    try catch

---
+++ by-john-smilga-ts/ajax/fetch/fetch-async-try-catch.ts
@@ -0,0 +1,11 @@
+const url = "./api/people.json";
+
+const btn = document.querySelector(".btn") as HTMLButtonElement;
+
+try {
+  console.log("hello");
+} catch (error) {
+  console.log(error);
+}
+
+console.log("hey");
