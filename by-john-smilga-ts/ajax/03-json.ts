b8026adf9dccbc1aa84750bdb84755bed4af4935
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-17 22:25:52 +0530

    json

--- by-john-smilga-ts/ajax/03-json.ts
+++ by-john-smilga-ts/ajax/03-json.ts
@@ -13,6 +13,24 @@ function getData(url) {
   xhr.onreadystatechange = function () {
     // console.log(xhr);
     if (xhr.readyState === 4 && xhr.status === 200) {
+      // parse
+      // stringify
+
+      const data = JSON.parse(xhr.responseText);
+      const displayData = data
+        .map((item) => {
+          return `<p>${item.name}</p>`;
+        })
+        .join("");
+      const element = document.createElement("div");
+      element.innerHTML = displayData;
+      document.body.appendChild(element);
+    } else {
+      console.log({
+        status: xhr.status,
+        text: xhr.statusText,
+        state: xhr.readyState,
+      });
     }
   };
   xhr.send();
