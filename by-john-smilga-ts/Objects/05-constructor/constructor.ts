2a9fb82333a84440f2fc750d1bfe81ed52dc293c
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-07-18 23:02:07 +0530

    constructor ts

--- by-john-smilga-ts/Objects/05-constructor/constructor.ts
+++ by-john-smilga-ts/Objects/05-constructor/constructor.ts
@@ -1,26 +1,42 @@
-// All Objects in Javascript have access to constructor property that returns a constructor function that created it.
-// built in constructor functions
-// arrays and functions are objects in javascript
+interface Person {
+  firstName: string;
+  lastName: string;
+  fullName: () => void;
+}
 
-function Person(firstName, lastName) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.fullName = function () {
+const john: Person = {
+  firstName: "john",
+  lastName: "anderson",
+  fullName: function () {
     console.log(
-      `My full name is ${this.firstName} ${this.lastName} and I love React`
+      `My full name is ${this.firstName} ${this.lastName} and I love JS`
     );
+  },
+};
+
+const bob: Person = {
+  firstName: "peter",
+  lastName: "sanders",
+  fullName: function () {
+    console.log(`My full name is ${this.firstName} ${this.lastName}`);
+  },
+};
+
+function createPerson(firstName: string, lastName: string): Person {
+  return {
+    firstName: firstName,
+    lastName: lastName,
+    fullName: function () {
+      console.log(
+        `My full name is ${this.firstName} ${this.lastName} and I love JS`
+      );
+    },
   };
 }
 
-const john = new Person("john", "sanders");
-// console.log(john.constructor);
-
-const bob = {};
-console.log(bob.constructor);
-const list = [];
-console.log(list.constructor);
-const sayHi = function () {};
-console.log(sayHi.constructor);
-
-const susy = new john.constructor("susy", "carpenter");
+const John: Person = createPerson("john", "anderson");
+John.fullName();
+const Bob: Person = createPerson("susy", "apple");
+Bob.fullName();
+const susy: Person = createPerson("bob", "jordan");
 susy.fullName();
